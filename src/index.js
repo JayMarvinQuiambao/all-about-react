d1c8ba5063aaf2c97b6560885a8784d844ad59ea
Quiambao <69567704+JayMarvinQuiambao@users.noreply.github.com>
2020-10-01 10:37:32 +0800

    Parent Child Component Exercise

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import MyInfo from './components/MyInfo';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyInfo />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
